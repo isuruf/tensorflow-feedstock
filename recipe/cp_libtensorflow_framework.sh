040b102cc7e4f1e15981a0a6bf251e6217895ca5
Isuru Fernando <isuruf@gmail.com>
2025-06-17 14:56:18 -0700

    fix files

--- recipe/cp_libtensorflow_framework.sh
+++ recipe/cp_libtensorflow_framework.sh
@@ -3,10 +3,10 @@ mkdir -p ${PWD}/tarball
 tar -C ${PWD}/tarball -xzf $SRC_DIR/libtensorflow.tar.gz
 mkdir -p ${PREFIX}/lib
 if [[ "$target_platform" == "osx-"* ]]; then
-  mv ${PWD}/tarball/lib/libtensorflow.*.dylib ${PREFIX}/lib
+  mv ${PWD}/tarball/lib/libtensorflow_framework.*.dylib ${PREFIX}/lib
 else
-  mv ${PWD}/tarball/lib/libtensorflow.so* ${PREFIX}/lib
+  mv ${PWD}/tarball/lib/libtensorflow_framework.so* ${PREFIX}/lib
 fi
 
 # Make writable so patchelf can do its magic
-chmod u+w $PREFIX/lib/libtensorflow*
+chmod u+w $PREFIX/lib/libtensorflow_framework*
